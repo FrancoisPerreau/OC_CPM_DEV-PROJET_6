8490387fe0c75c2e1288a56497eb73d2198aa010
FrancoisPerreau <f.perreau@orange.fr>
2019-04-23 16:34:20 +0200

    Initialiastion du projet symfony 3.4

---
+++ projet_6/README.md
@@ -0,0 +1,4 @@
+projet_6
+========
+
+A Symfony project created on April 23, 2019, 2:13 pm.
---
+++ projet_6/src/CitrespBundle/CitrespBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CitrespBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CitrespBundle extends Bundle
+{
+}
